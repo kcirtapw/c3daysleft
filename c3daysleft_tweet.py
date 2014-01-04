b931881fa7981b90984a9dd13ed2a98aea6962b2
Kerstin Kollmann <code@kerstinkollmann.com>
2014-01-04 17:33:11 +0100

    fixed grammar, punctuation, style

--- c3daysleft_tweet.py
+++ c3daysleft_tweet.py
@@ -10,15 +10,15 @@ TWEET_TEXTS=(
 	"Only %s days left until #31C3",
 	"Only %s days left until #31C3",
 	"Only %s days left until #31C3",
-	"Goooood Mooooorning Twietnaaam! On this lovely day, there are only %s days left until #31C3",
-	"What a nice day! And wow, only %s remain until #31C3 starts! #anticipation",
-	"I'm very pleased to remember you, that the #31C3 will be in only %s days!",
-	"Bugged by all those strange people out there? Heads up, #31C3 will start in only %s days!! :)",
-	"%s days remaining!! wooohoooo :D #31C3",
+	"Goooood Mooooorning, Twietnaaam! On this lovely day, there are only %s days left until #31C3",
+	"What a nice day! And wow, only %s days remain until #31C3 starts! #anticipation",
+	"I'm very pleased to remind you that #31C3 will take place in only %s days!",
+	"Bugged by all those strange people out there? Chin up, #31C3 will start in only %s days!! :)",
+	"%s days remaining!! Wooohoooo :D #31C3",
 	)
 TWEET_SPECIAL={
-	356: "So, the first sunday after the #30C3. Is your sleep cycle still as fucked up as mine? We have 356 days to recover until #31C3 starts :D",
-	348: "Day 4 of the #30C3 is already two weeks ago! That leaves us only 355 days left until #31C3 \o/",
+	356: "So, the first Sunday after #30C3. Is your sleep cycle still as out of whack as mine? We have 356 days to recover until #31C3 starts :D",
+	348: "Day 4 of #30C3 was two weeks ago! That leaves only 355 days until #31C3 \o/",
 	341: "Oh yea, only b'101010101 days left until #31C3",
 	}
 MIN_SLEEP=60*30
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         mylog("sleeping for %ss" % sleeptime)
         sleep(sleeptime)
     oauth=twitter.read_token_file(OAUTH_FILE)
-    t=twitter.Twitter(auth=twitter.OAuth(oauth[0], oauth[1], 
+    t=twitter.Twitter(auth=twitter.OAuth(oauth[0], oauth[1],
 	bot_apikeys.CONSUMER_KEY, bot_apikeys.CONSUMER_SECRET))
     daysleft = (NEXTC3_DATE - datetime.date.today()).days
     if daysleft in TWEET_SPECIAL:
